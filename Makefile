e023716a882d288da8a8ae78f403c83675540e27
Anton Berezin <tobez@tobez.org>
2014-05-02 22:58:29 +0200

    Beginnings of the actual painting.

--- Makefile
+++ Makefile
@@ -1,16 +1,18 @@
 OPTIMIZE?=-O2 -g
 CFLAGS?=-Wall -Werror -fno-strict-aliasing
+CAIRO_CFLAGS != pkg-config --cflags gtk+-3.0
+CAIRO_LFLAGS != pkg-config --libs gtk+-3.0
 CC?=cc
 
 citaa: main.o carp.o read.o image.o graph.o trace.o
-	$(CC) $(CFLAGS) $(OPTIMIZE) -o citaa \
+	$(CC) $(CFLAGS) $(OPTIMIZE) $(CAIRO_LFLAGS) -o citaa \
 	    main.o carp.o read.o image.o graph.o trace.o
 
 clean:
 	rm citaa main.o carp.o read.o image.o graph.o
 
 main.o: main.c citaa.h
-	$(CC) -c $(CFLAGS) $(OPTIMIZE) -o main.o main.c
+	$(CC) -c $(CFLAGS) $(OPTIMIZE) $(CAIRO_CFLAGS) -o main.o main.c
 
 carp.o: carp.c citaa.h
 	$(CC) -c $(CFLAGS) $(OPTIMIZE) -o carp.o carp.c
--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include <limits.h>
 #include <ctype.h>
 
+#include <cairo.h>
+
 #include "citaa.h"
 #include "bsdqueue.h"
 
@@ -617,6 +619,131 @@ create_text(int y, int x, CHAR *text)
 	return t;
 }
 
+struct schema {
+	int xscale;
+	int yscale;
+	int border_left;
+	int border_right;
+	int border_top;
+	int border_bottom;
+} default_paint_schema = {
+	.xscale = 10,
+	.yscale = 14,
+	.border_left = 20,
+	.border_right = 20,
+	.border_top = 28,
+	.border_bottom = 28,
+};
+
+struct paint_context {
+	cairo_surface_t *surface;
+	cairo_t *cr;
+	struct schema *s;
+
+	int i_width;
+	int i_height;
+
+	int o_width;
+	int o_height;
+	int o_x;
+	int o_y;
+};
+
+#define pcx(x) pc->o_x + x * pc->s->xscale
+#define pcy(y) pc->o_y + y * pc->s->yscale
+
+void
+paint_box(struct paint_context *pc, struct component *c)
+{
+	struct vertex *v0, *start, *v1;
+	int i, dir, new_dir;
+
+	cairo_set_line_width(pc->cr, 1);
+	cairo_set_line_cap(pc->cr, CAIRO_LINE_CAP_ROUND);
+	cairo_set_source_rgb(pc->cr, 0, 0, 0);
+
+	start = TAILQ_FIRST(&c->vertices);
+	cairo_move_to(pc->cr, pcx(start->x), pcy(start->y));
+
+	v0 = start;
+	for (dir = 0; dir < N_DIRECTIONS; dir++)
+		if (v0->e[dir])	break;
+
+	while (1) {
+		v1 = v0->e[dir];
+
+		cairo_line_to(pc->cr, pcx(v1->x), pcy(v1->y));
+		if (v1 == start)
+			break;
+
+		for (i = 1; i >= -1; i--) {
+			new_dir = (dir + i + 4) % N_DIRECTIONS;
+			if (v1->e[new_dir]) {
+				dir = new_dir;
+				v0 = v1;
+				break;
+			}
+		}
+	}
+
+	cairo_stroke_preserve(pc->cr);
+	if (c->has_custom_background)
+		cairo_set_source_rgb(pc->cr,
+							 c->custom_background.r / 15.0,
+							 c->custom_background.g / 15.0,
+							 c->custom_background.b / 15.0);
+	else
+		cairo_set_source_rgb(pc->cr, 1, 1, 1);
+	cairo_fill(pc->cr);
+}
+
+void
+paint_line(struct paint_context *pc, struct component *c)
+{
+}
+
+void
+paint_component(struct paint_context *pc, struct component *c)
+{
+	if (c->type == CT_BOX)
+		paint_box(pc, c);
+	else if (c->type == CT_LINE)
+		paint_line(pc, c);
+}
+
+void
+paint(int i_height, int i_width)
+{
+	struct paint_context ctx;
+	struct paint_context *pc = &ctx;
+	struct component *c;
+
+	pc->s = &default_paint_schema;
+	pc->i_width = i_width;
+	pc->i_height = i_height;
+	pc->o_width = pc->s->border_left + pc->i_width * pc->s->xscale + pc->s->border_right;
+	pc->o_height = pc->s->border_top + pc->i_height * pc->s->yscale + pc->s->border_bottom;
+	pc->o_x = pc->s->border_left + pc->s->xscale / 2;
+	pc->o_y = pc->s->border_top + pc->s->yscale / 2;
+
+	pc->surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, pc->o_width, pc->o_height);
+	pc->cr = cairo_create(pc->surface);
+
+	cairo_set_source_rgb(pc->cr, 1, 1, 1);
+	cairo_rectangle(pc->cr, 0, 0, pc->o_width, pc->o_height);
+	cairo_fill(pc->cr);
+
+	TAILQ_FOREACH(c, &components, list) {
+		paint_component(pc, c);
+	}
+	// paint free text
+
+	cairo_surface_write_to_png(pc->surface, "o.png");
+
+	cairo_destroy(pc->cr);
+	cairo_surface_destroy(pc->surface);
+}
+
 int
 main(int argc, char **argv)
 {
@@ -653,24 +780,7 @@ main(int argc, char **argv)
 
 	extract_text(orig);
 
-	TAILQ_FOREACH(c, &components, list) {
-		dump_component(c);
-	}
-
-	if (0) {
-		find_enclosing_component(&components, 1, 2);
-		find_enclosing_component(&components, 4, 6);
-		find_enclosing_component(&components, 4, 5);
-		find_enclosing_component(&components, 3, 5);
-		find_enclosing_component(&components, 3, 6);
-		find_enclosing_component(&components, 2, 20);
-		find_enclosing_component(&components, 2, 0);
-		find_enclosing_component(&components, 6, 0);
-		find_enclosing_component(&components, 6, 1);
-		find_enclosing_component(&components, 6, 5);
-		find_enclosing_component(&components, 6, 10);
-		find_enclosing_component(&components, 7, 2);
-	}
+	paint(orig->h, orig->w);
 
 	return 0;
 
